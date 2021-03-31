53aabb11a20b0d7bb752ac9d34a7f0a157a3609b
github username <rvtingling@yahoo.com>
2021-03-30 21:46:31 -0400

    created index action for quote types

--- app/controllers/quote_types_controller.rb
+++ app/controllers/quote_types_controller.rb
@@ -1,2 +1,8 @@
 class QuoteTypesController < ApplicationController
+
+    def index 
+        quoteTypes = QuoteType.all
+        render json: quoteTypes, include: [:freight_quotes]
+    end 
+
 end
