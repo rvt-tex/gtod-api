ea99a7c5f1dc147f6eb54e70c1e387f43117ac1b
github username <rvtingling@yahoo.com>
2021-03-30 22:17:07 -0400

    updated controllers with crud methods

--- app/controllers/freight_quotes_controller.rb
+++ app/controllers/freight_quotes_controller.rb
@@ -1,2 +1,38 @@
 class FreightQuotesController < ApplicationController
+
+    def index
+        freight_quotes = FreightQuote.all 
+        render json: freight_quotes
+        # render json: PinSerializer.new(pins)
+    end 
+
+    def create 
+        freight_quote = FreightQuote.new(freight_quote_params)
+        if freight_quote.save 
+            # render json: FreightQuoteSerializer.new(freight_quote)
+        else 
+            render json: {errors: freight_quote.errors.full_messages}, status: :unprocessible_entity
+        end 
+    end 
+
+    def destroy 
+        freight_quote = FreightQuote.find_by_id(params[:id])
+        if freight_quote.destroy
+            freight_quotes = FreightQuote.all 
+            render json: freight_quotes
+            # render json: FreightQuoteSerializer.new(freight_quotes)
+        else
+            render json: { message: "error"}  
+        end 
+    end 
+
+    private
+
+    def freight_quote_params
+        params.require(:freight_quote).permit(:first_name, :last_name, :company, :email, :phone, :description, :quote_type_id, :shipper_id)
+    end 
+
+    def find_freight_quote
+        @pfreight_quote = FreightQuote.find_by_id(params[:id])
+    end
 end
--- app/controllers/shippers_controller.rb
+++ app/controllers/shippers_controller.rb
@@ -1,2 +1,38 @@
 class ShippersController < ApplicationController
+
+    def index
+        shippers = Shipper.all 
+        render json: shippers, include: [:freight_quotes]
+        # render json: PinSerializer.new(pins)
+    end 
+
+    def create 
+        shipper = Shipper.new(shipper_params)
+        if shipper.save 
+            # render json: FreightQuoteSerializer.new(freight_quote)
+        else 
+            render json: {errors: shipper.errors.full_messages}, status: :unprocessible_entity
+        end 
+    end 
+
+    def destroy 
+        shipper = Shipper.find_by_id(params[:id])
+        if shipper.destroy
+            shippers = Shipper.all 
+            render json: shippers
+            # render json: FreightQuoteSerializer.new(freight_quotes)
+        else
+            render json: { message: "error"}  
+        end 
+    end 
+
+    private
+
+    def shipper_params
+        params.require(:shipper).permit(:first_name, :last_name, :title, :department, :company, :address, :city, :state, :zip_code, :email, :phone, :website)
+    end 
+
+    def find_shipper
+        @pshipper = Shipper.find_by_id(params[:id])
+    end
 end
